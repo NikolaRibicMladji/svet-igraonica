bfe1badff2c9c1f73cad9dda241325c583a0ee8c
Nikola Ribic Mladji <ribinator5@gmil.com>
2026-03-29 19:47:51 +0200

    povezan frontend sa render api-jem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Baza URL za sve API pozive
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://svet-igraonica.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
