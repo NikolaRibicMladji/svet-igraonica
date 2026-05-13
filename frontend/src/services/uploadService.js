f88cc64cf04f36ea82d509ac6a31275d1f8ed897
Nikola Ribic Mladji <tvoj-ispravan-email>
2026-05-13 15:15:27 +0200

    fix mobile multipart upload

--- frontend/src/services/uploadService.js
+++ frontend/src/services/uploadService.js
@@ -4,7 +4,11 @@ export const uploadImage = async (file) => {
   const formData = new FormData();
   formData.append("image", file);
 
-  const res = await api.post("/temp-upload/temp", formData);
+  const res = await api.post("/temp-upload/temp", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
   return res.data.data;
 };
